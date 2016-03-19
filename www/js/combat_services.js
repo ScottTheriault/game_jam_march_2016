f210c9d1f1bd81c84c69b1b2489a6af37d924c9f
ScottTheriault <Scott.Theriault@hotmail.com>
2016-03-19 16:46:59 -0300

    added turns icons

--- www/js/combat_services.js
+++ www/js/combat_services.js
@@ -12,34 +12,36 @@ angular.module('game_jam.combat_services', ['common.services'])
 
 		if (turns.length) {
 			turn.type = PLAYER;
-			turn.player = player_services.getPlayers[0];
+			turn.player = player_services.getPlayers()[0];
+			console.log(turn);
 		} else {
 			var nextPlayer = undefined;
 			if (player.type === PLAYER) {
 				var id = player.id;
-				nextPlayer = player_services.getEnemies[id];
+				nextPlayer = player_services.getEnemies()[id];
 				turn.type = ENEMY;
 				if (typeof nextPlayer === 'undefined') {
 					turn.type = PLAYER;
-					nextPlayer = player_services.getPlayers[id+1];
+					nextPlayer = player_services.getPlayers()[id+1];
 				}
 			} else {
 				var id = player.id + 1;
-				nextPlayer = player_services.getPlayers[0];
+				nextPlayer = player_services.getPlayers()[0];
 				turn.type = PLAYER;
 				if (typeof nextPlayer === 'undefined') {
-					nextPlayer = player_services.getEnemies[id];
+					nextPlayer = player_services.getEnemies()[id];
 					turn.type = ENEMY;
 				}
 			}
 
 			if (typeof nextPlayer === 'undefined') {
-				nextPlayer = player_services.getPlayers[0];
+				nextPlayer = player_services.getPlayers()[0];
 				turn.type = PLAYER;
 			}
 			turn.player = nextPlayer;
 		}
 
+		turn.players = [turn.player];
 		turns.push(turn);
 	}
 
