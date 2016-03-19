2a9458f6c30a047ffe4f0cea20022aa1c2966376
ScottTheriault <Scott.Theriault@hotmail.com>
2016-03-19 12:58:41 -0300

    added background to combat, kinda

--- www/js/combat_services.js
+++ www/js/combat_services.js
@@ -1,6 +1,14 @@
 angular.module('game_jam.combat_services', ['common.services'])
 
 .factory('combat_services', ['utility', function(utility) {
+	var combat = {};
+
 	return {
+		newCombat: function(backgroundImg) {
+			combat =
+				{
+					backgroundImage: backgroundImg
+				};
+		}
 	}
 }]);
--- www/js/controllers/combat-controller.js
+++ www/js/controllers/combat-controller.js
@@ -1,7 +1,8 @@
 angular.module('game_jam.controllers')
 
-.controller('CombatCtrl', function($scope, player_services) {
+.controller('CombatCtrl', function($scope, combat_services, player_services) {
 	player_services.setNewPlayer();
+	combat_services.newCombat('img/background/combat/simple_test.png');
 
 	$scope.testIt = function () {
 		return player_services.getPlayers();
