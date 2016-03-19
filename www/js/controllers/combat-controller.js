f47ded98b1dfbb2376289ea6078ce09e9c4e989a
ScottTheriault <Scott.Theriault@hotmail.com>
2016-03-19 14:54:28 -0300

    players are now orange blocks

--- www/js/controllers/combat-controller.js
+++ www/js/controllers/combat-controller.js
@@ -2,9 +2,15 @@ angular.module('game_jam.controllers')
 
 .controller('CombatCtrl', function($scope, combat_services, player_services) {
 	player_services.setNewPlayer();
+	player_services.setNewPlayer();
+	player_services.setNewPlayer();
 	combat_services.newCombat('img/background/combat/simple_test.png');
 
 	$scope.getBackgroundImg = function() {
 		return combat_services.getBackgroundImg();
+	}
+
+	$scope.getPlayers = function() {
+		return player_services.getPlayers();
 	}
 });
