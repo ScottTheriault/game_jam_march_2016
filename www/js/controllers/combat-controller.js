907fbaee51dd2571ebf8c98deebda2d3b0783722
ScottTheriault <Scott.Theriault@hotmail.com>
2016-03-20 16:20:36 -0300

    added fight instance

--- www/js/controllers/combat-controller.js
+++ www/js/controllers/combat-controller.js
@@ -1,19 +1,9 @@
 angular.module('game_jam.controllers')
 
 .controller('CombatCtrl', function($scope, combat_services, player_services, enemy_services) {
-	/*player_services.setKnight();
-	player_services.setRanger();
-	player_services.setMage();
-
-	player_services.addEnemyPlayer(enemy_services.getById(0));
-	player_services.addEnemyPlayer(enemy_services.getById(0));
-	player_services.addEnemyPlayer(enemy_services.getById(0));*/
-
 	$scope.MOVE_ATTACK = combat_services.attackToggle();
 	$scope.MOVE_FIRE = combat_services.fireToggle();
 	$scope.MOVE_LIGHTNING = combat_services.lightningToggle();
-
-	combat_services.newCombat('img/background/combat/background_forest.png');
 
 	$scope.getBackgroundImg = function() {
 		return combat_services.getBackgroundImg();
--- www/js/level_services.js
+++ www/js/level_services.js
@@ -1,13 +1,16 @@
 angular.module('game_jam.level_services', ['common.services'])
 
-.factory('level_services', ['utility', 'text_services', 'walking_services', function(utility, text_services, walking_services) {
+.factory('level_services', ['utility', 'text_services', 'walking_services', 'player_services', 'combat_services', 'enemy_services', function(utility, text_services, walking_services, player_services, combat_services, enemy_services) {
 	var TEXT_VIEW = 'TEXT_VIEW';
 	var WALKING = 'WALKING';
+	var COMBAT = 'COMBAT';
+
 	var levels =
 		[
 			{type: TEXT_VIEW, text_index: 0},
 			{type: WALKING, walking_index: 0},
-			{type: WALKING, walking_index: 1}
+			{type: WALKING, walking_index: 1},
+			{type: COMBAT, backgroundImage: 'img/background/combat/background_forest.png', enemy_ids: [1]}
 		];
 	var index = -1;
 
@@ -19,6 +22,18 @@ angular.module('game_jam.level_services', ['common.services'])
 	function nextWalking() {
 		walking_services.setByIndex(levels[index].walking_index);
 		return 'tab.walk';
+	}
+
+	function nextCombat() {
+		var level = levels[index];
+		player_services.clearEnemies();
+
+		for (var i = 0; i < level.enemy_ids.length; i++) {
+			player_services.addEnemyPlayer(enemy_services.getById(level.enemy_ids[i]));
+		}
+
+		combat_services.newCombat(level.backgroundImage);
+		return 'tab.combat';
 	}
 
 	return {
@@ -34,6 +49,8 @@ angular.module('game_jam.level_services', ['common.services'])
 					return nextTextView();
 				case WALKING:
 					return nextWalking();
+				case COMBAT:
+					return nextCombat();
 			}
 		}
 	}
