73e9eef581196f23229aa27af58606139d0d74c1
ScottTheriault <Scott.Theriault@hotmail.com>
2016-03-20 13:56:44 -0300

    main menu looks pretty

--- www/js/controllers/main_menu_controller.js
+++ www/js/controllers/main_menu_controller.js
@@ -1,4 +1,13 @@
 angular.module('game_jam.controllers')
 
 .controller('MainMenuCtrl', function($scope) {
+	$scope.showLoadGames = false;
+
+	$scope.getBackgroundImg = function() {
+		return 'img/background/combat/background_forest.png';
+	}
+
+	$scope.toggleShowLoadGames = function() {
+		$scope.showLoadGames^=1;
+	}
 });
