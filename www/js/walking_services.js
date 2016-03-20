adb8c57cdb369bb0d03c98ba879bb50365623c89
ScottTheriault <Scott.Theriault@hotmail.com>
2016-03-20 15:10:16 -0300

    added walking services

---
+++ www/js/walking_services.js
@@ -0,0 +1,6 @@
+angular.module('game_jam.walking_services', ['common.services', 'game_jam.item_services'])
+
+.factory('walking_services', ['utility', 'item_services', function(utility, item_services) {
+	return {
+	}
+}]);
