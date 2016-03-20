366018a79e28e2c929faff037fdc205969a62cae
ScottTheriault <Scott.Theriault@hotmail.com>
2016-03-20 14:11:26 -0300

    added new game text

--- www/js/text_services.js
+++ www/js/text_services.js
@@ -1,6 +1,15 @@
 angular.module('game_jam.text_services', ['common.services'])
 
 .factory('text_services', ['utility', function(utility) {
+	var textStream = [];
+	var index = 0;
 	return {
+		setNewGameText: function() {
+			textStream = [
+				"Welcome Sir. Edward, the mythical dragon Qenoff is attacking! Take your brother Alphonse and the trusted LaFawnduh",
+				"Only with the power of teamwork will you manage to defeat Qenoff"
+			]
+			index = 0;
+		}
 	}
 }]);
