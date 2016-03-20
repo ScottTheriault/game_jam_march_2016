dd57d7083423662a64dfb554b6e6c6b13b37266e
ScottTheriault <Scott.Theriault@hotmail.com>
2016-03-20 15:35:39 -0300

    now shows the player in walk scene

--- www/js/controllers/walk_controller.js
+++ www/js/controllers/walk_controller.js
@@ -1,7 +1,14 @@
 angular.module('game_jam.controllers')
 
-.controller('WalkCtrl', function($scope, walking_services) {
+.controller('WalkCtrl', function($scope, walking_services, player_services) {
 	$scope.getBackgroundImg = function() {
 		return walking_services.getBackgroundImg();
+	}
+
+	$scope.getPlayerArray = function() {
+		return [player_services.getPlayers()[0]];
+	}
+
+	$scope.next = function() {
 	}
 });
